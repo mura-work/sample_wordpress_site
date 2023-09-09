29f5373cca5b5320e29ab65c14b5b200b9afd8b2
mura-work <murata.naoya.work@gmail.com>
2023-09-09 10:05:52 +0900

    [feat] function.phpに処理の切り出し

--- single.php
+++ single.php
@@ -1,47 +1,50 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 
 <head>
   <?php get_header(); ?>
 </head>
 
-<body>
+<body <?php body_class(); ?>>
   <?php get_template_part('includes/header'); ?>
 
   <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
+    <?php while (have_posts()) : the_post(); ?>
 
-  <!-- Page Header -->
-  <header class="masthead" style="background-image: url('img/post-bg.jpg')">
-    <div class="overlay"></div>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <div class="post-heading">
-            <h1><?php the_title(); ?></h1>
-            <span class="meta">Posted by
-              <?php the_author(); ?>
-              on <?php the_date(); ?></span>
+      <?php $img = get_eyecatch_with_default(); ?>
+
+      <!-- Page Header -->
+      <header class="masthead" style="background-image: url('<?php echo $img[0]; ?>')">
+        <div class="overlay"></div>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <div class="post-heading">
+                <h1><?php the_title(); ?></h1>
+                <span class="meta">Posted by
+                  <?php the_author(); ?>
+                  on <?php the_date(); ?></span>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </header>
+      </header>
 
-  <!-- Post Content -->
-  <article>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <?php the_content(); ?>
+      <!-- Post Content -->
+      <article>
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 col-md-10 mx-auto">
+              <?php the_post_thumbnail(array(400, 200)); ?>
+              <?php the_content(); ?>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </article>
+      </article>
 
-  <hr>
+      <hr>
 
-  <?php endwhile; ?>
+    <?php endwhile; ?>
   <?php endif; ?>
 
   <?php get_template_part('includes/footer'); ?>
